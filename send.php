466deaa8f5be170b21de389281f6daf9ef2733cc
Ben Dreamer <bendjamin18@gmail.com>
2016-04-21 23:11:19 +0300

    Сделаны доработки(уменьшен шрифт, прописаны названия страниц)

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Sunset site</title>
+	<title>Thank you!</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
